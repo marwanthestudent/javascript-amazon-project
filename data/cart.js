d749d7621d9625b535b7141946d2c3f14b05a32d
marwan <abulibdeh43@gmail.com>
2024-07-16 14:24:44 -0500

    remove product from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,22 +1,32 @@
-export const cart = [{
-    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
+export let cart = [{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
 },{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
 }];
 
 export function addToCart(productId){
-    let matchingItem;
-    cart.forEach((cartItem)=>{
-      if(productId === cartItem.productId) matchingItem = cartItem;
-    }); 
-  
-    if (matchingItem) matchingItem.quantity++;
-    else{
-      cart.push({
-        productId,
-        quantity: 1
-      });
-    }
+  let matchingItem;
+  cart.forEach((cartItem)=>{
+    if(productId === cartItem.productId) matchingItem = cartItem;
+  }); 
+
+  if (matchingItem) matchingItem.quantity++;
+  else{
+    cart.push({
+      productId,
+      quantity: 1
+    });
   }
+}
+
+export function removeFromCart(productId){
+  const newCart = [];
+  //cart.splice(cart.indexOf(productId),1); IDK why it's not working
+  cart.forEach(cartItem =>{
+    if(cartItem.productId !== productId) newCart.push(cartItem);
+  })
+
+  cart = newCart;
+}
