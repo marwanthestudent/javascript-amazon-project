905851fb42c74adeb8e0cbac11623d49af1ec348
marwan <abulibdeh43@gmail.com>
2024-07-31 16:18:50 -0500

    converted products into classes

--- data/products.js
+++ data/products.js
@@ -1,9 +1,36 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId){
   let matchingProduct;
     products.forEach(product => {
       if(product.id === productId) matchingProduct = product;
     });
     return matchingProduct;
+}
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+    
 }
 
 export const products = [
@@ -665,4 +692,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
